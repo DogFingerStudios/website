950b107f4e5c26a8d63df54adf1cf0436d74880e
Addy <aclaure@gmail.com>
2020-09-30 20:40:57 -0400

    initial commit

---
+++ downloads/index.php
@@ -0,0 +1,136 @@
+<?php
+
+define("PRODUCTION", 1);
+
+if (PRODUCTION == 1)
+{
+    define("DOWNLOAD_PATH", "./downloads/");
+    define('LOCAL_PATH_PREFIX', "/amb/public_html/");
+}
+else
+{
+    define("DOWNLOAD_PATH", "./");
+    define('LOCAL_PATH_PREFIX', "");
+}
+
+function getWindowsDownloadFilename()
+{
+    $dir = glob(LOCAL_PATH_PREFIX . DOWNLOAD_PATH . 'tooter-*.exe');
+    usort($dir, create_function('$a,$b', 'return filemtime($b) - filemtime($a);'));
+    if (count($dir) > 0)
+    {
+        return basename($dir[0]);
+    }
+
+    return '';
+}
+
+function getMacDownloadFilename()
+{
+    $dir = glob(LOCAL_PATH_PREFIX . DOWNLOAD_PATH . 'tooter-*-darwin*.zip');
+    usort($dir, create_function('$a,$b', 'return filemtime($b) - filemtime($a);'));
+    if (count($dir) > 0)
+    {
+        return basename($dir[0]);
+    }
+
+    return '';
+}
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>The Tommy Tucson Video Game</title>
+    <!-- <link rel="icon" type="img/png" href="images/owl_32.png" /> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      @font-face {
+        font-family: 'Open Sans';
+        font-style: normal;
+        font-weight: 300;
+        src: local('Open Sans Light'), local('OpenSans-Light'), url(fonts/DXI1ORHCpsQm3Vp6mXoaTXhCUOGz7vYGh680lGh-uXM.woff) format('woff');
+      }
+
+      @font-face {
+        font-family: 'Open Sans';
+        font-style: normal;
+        font-weight: 400;
+        src: local('Open Sans'), local('OpenSans'), url(fonts/cJZKeOuBrn4kERxqtaUH3T8E0i7KZn-EPnyo3HZu7kw.woff) format('woff');
+      }
+
+      body, h1, h2, h3, h4, p 
+      {
+        font-family: "Helvetica Neue",Helvetica,Arial,sans-serif !important;
+        font-weight: 300;
+        margin: 5px;
+        padding: 5px;
+      }
+    </style>
+  
+  </head>
+  <body>
+
+<center>
+    <h1>The Tommy Tucson Video Game</h1>
+    <br/>
+    <a href="#about">About</a>&nbsp;&nbsp;&nbsp;
+    <a href="https://github.com/zethon/ttvg/wiki" target="_blank">Wiki</a>&nbsp;&nbsp;&nbsp;
+    <a href="https://github.com/zethon/ttvg" target="_blank">GitHub</a>&nbsp;&nbsp;&nbsp;
+    <a href="#downloads">Download</a>&nbsp;&nbsp;&nbsp;
+    <a href="https://www.youtube.com/channel/UC817umzuXFvE18cnsVx9JuA/videos" target="_blank">Videos</a>
+    <br/>
+    <br/>
+    <img src="https://github.com/zethon/ttvg/raw/master/docs/images/screenshot1.png"/ width="55%">
+</center>
+
+<br/></br>
+
+<a id="about"/><h2>About</h2>
+
+<p>The Tommy Tucson Video Game is an open source role-playing game based on the fictional character Tommy Tucson. All characters and events in this video game, even those based on real people, are entirely fictional. This video game contains coarse language and due to its content it should not be played by anyone.</p>
+
+<br/>
+
+<a id="downloads"/><h2>Downloads</h2>
+
+<h3>Windows</h3>
+
+<!-- Download Path: <?php echo DOWNLOAD_PATH; ?> -->
+<!-- Prefix Path  : <?php echo LOCAL_PATH_PREFIX; ?> -->
+
+<p> 
+<?php 
+    $filename = getWindowsDownloadFilename();
+    echo '<a href="http://amb.la/' . DOWNLOAD_PATH . $filename . '">' . $filename . '</a>';
+    $filetime = date("d F Y H:i", filemtime(LOCAL_PATH_PREFIX . DOWNLOAD_PATH . $filename));
+    echo '&nbsp;(' . $filetime . ' GMT)';
+?>
+</p>
+
+<br/>
+
+<h3>macOS</h3>
+<p> 
+<?php 
+    $filename = getMacDownloadFilename();
+    echo '<a href="http://amb.la/' . DOWNLOAD_PATH . $filename . '">' . $filename . '</a>';
+    $filetime = date("d F Y H:i", filemtime(LOCAL_PATH_PREFIX . DOWNLOAD_PATH . $filename));
+    echo '&nbsp;(' . $filetime . ' GMT)';
+?>
+</p>
+
+<br/>
+<h3>Ubuntu</h3>
+<p>Coming soon...</p>
+<br/>
+
+</body>
+
+<script src="https://code.jquery.com/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</html>
---
+++ index.php
@@ -0,0 +1,65 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-55358451-2"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-55358451-2');
+    </script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Cutive+Mono" rel="stylesheet">
+    <meta name="description" content="Dog Finger Studios is a new indie game studio. We are currently developing the Tommy Tucson Series" />
+    <meta name="keywords" content="Game, RPG, Tommy Tucson">
+
+</head>
+
+<body>
+
+<!--INTRO_COPY-->
+<section>
+    <div class="intro_copy_container">
+    <h1 class="title_one">Dog Finger Studios</h1>
+    <div class="social_c">
+        <a href="https://www.youtube.com/channel/UC817umzuXFvE18cnsVx9JuA" target="_blank">YouTube</a> 
+    </div>
+        <!-- <p>Dog Finger Studios was founded in 2020 by Kevin Hoffman and Associates as an independent game studio dedicated to producing video games. We</p> -->
+    </div>
+</section>
+<!--INTRO_COPY_END-->
+
+<!--2_COLUMN_LAYOUT-->
+<!--1ST_ROW-->
+<section class="two_sec_row">
+    <div class="img_container_1">
+        <img class="img_1" src="https://github.com/zethon/ttvg/raw/master/docs/images/screenshot1.png" alt="hallway" >
+    </div>
+    <div class="text_block">
+        <div class="main_title"> <h2 class="general_text_rules titles"><a href="/downloads">The Tommy Tucson Video Game</a></h2></div>
+        <div class="std_text"><p class="general_text_rules">Tommy Tucson is a trans feminine intersex male wandering the streets of Tucson filing lawsuits, smoking weed and getting tard wrangled by the police. Do you have what it takes to run a successful rock selling business, defeat the Hoffman Gang and bring down the Internet terrorists?</p></div>
+    </div>
+</section>
+<!--1ST_ROW_END-->
+<!--2_COLUMN_LAYOUT_ENDS-->
+
+<!--FOOTER--> 
+<footer>
+    <div class="footer">
+        <p>Dog Finger Studios. All rights reserved&copy;</p>
+        <p>Contact us at info@dogfinger.com</p>
+    </div>
+    <div class="social_c">
+        <a href="https://www.youtube.com/channel/UC817umzuXFvE18cnsVx9JuA" target="_blank">YouTube</a> 
+    </div>
+</footer>
+<!--FOOTER_ENDS-->
+
+</body>
+</html>
